58a881e4c5f3232db8e42312251b692eb55889ec
yuxiaotu <yuxiaotu1024@qq.com>
2022-02-09 16:01:47 +0800

    css

---
+++ src/js/bubble.js
@@ -0,0 +1,16 @@
+const box = document.querySelector('.box');
+
+for (let i = 0; i < 100; i++) {
+  let node = document.createElement('div');
+  let left = Math.floor(Math.random() * 100);
+  let top = Math.floor(Math.random() * 100);
+  node.style.left = `${left}%`;
+  node.style.top = `${top}%`;
+  
+  let diameter = Math.floor(Math.random() * 50 + 10);
+  node.style.width = `${diameter}px`;
+  node.style.height = `${diameter}px`;
+
+  node.classList.add(...['bubble', `bgc-${Math.floor(Math.random() * 10)}`]);
+  box.appendChild(node);
+}
