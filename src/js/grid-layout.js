28db7a1f3812445e78ee531707aa277cec6ccd22
yuxiaotu <yuxiaotu1024@qq.com>
2022-07-20 15:39:24 +0800

    css learn

--- src/js/grid-layout.js
+++
@@ -1,8 +0,0 @@
-let box = document.querySelector('.item');
-let fragment = document.createDocumentFragment();
-for (let i = 0; i < 10; i++) {
-  let divNode = document.createElement('div');
-  divNode.classList.add(`bgc-${i+1}`);
-  fragment.appendChild(divNode);
-}
-box.appendChild(fragment);
